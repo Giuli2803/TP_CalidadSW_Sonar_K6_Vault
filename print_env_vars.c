639eb6fac82a0c5e43368673f1348993077490cc
Giuli2803 <giulianopalombarini@gmail.com>
2024-11-02 04:33:20 -0300

    agrego variables solo con variables externas

--- print_env_vars.c
+++ print_env_vars.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    const char* password = getenv("PASSWORD_OF_VAULT");
-    const char* username = getenv("USERNAME_OF_VAULT");
+    const char* password = PASSWORD_OF_VAULT;
+    const char* username = USERNAME_OF_VAULT;
 
-    if (password) {
-        printf("PASSWORD_OF_VAULT: %s\n", password);
-    } else {
-        printf("PASSWORD_OF_VAULT no está definida.\n");
-    }
-
-    if (username) {
-        printf("USERNAME_OF_VAULT: %s\n", username);
-    } else {
-        printf("USERNAME_OF_VAULT no está definida.\n");
-    }
+    printf("PASSWORD_OF_VAULT: %s\n", password);
+    printf("USERNAME_OF_VAULT: %s\n", username);
 
     return 0;
 }
